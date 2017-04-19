883c9a18ea4197d7f0fe2a03cad77882a215f006
Grant Lindberg <grant.lindberg@wsu.edu>
2017-04-19 14:16:33 -0700

    Modify parsing function to return a dictionary instead of a list

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import os
 import re
 
+import strategies
+
 def create_bag_of_words(document):
     """This function takes a preprocessed document as an argument and proceeds to create a dictionary, mapping each term to its frequency in the document. It returns a dictionary."""
     document = document.split()
@@ -17,7 +19,7 @@ def create_bag_of_words(document):
 
     return tokens
 
-def preprocess_document(document):
+def preprocess(document):
     """This function converts non-alphabetic characters to whitespace, lowercases all letters, and collapses all subsequent whitespace down to a single space. It returns a string of the preprocessed document"""
     document = re.sub(r'\W+', ' ', document)
     document = re.sub(r'\d+', ' ', document)
@@ -27,26 +29,34 @@ def preprocess_document(document):
 
     return document
 
-def parse_documents(direct):
-    """This function takes a directory as an argument and traverses each text file in the directory. With each text file, it will preprocess it. It returns void."""
+def parse_documents_from_directory(direct):
+    """This function takes a directory as an argument and traverses each text file in the directory. With each text file, it will preprocess it. It returns a dictionary of all the names of the preprocessed documents mapped to their corresponding text."""
     files = os.listdir(direct)
+    preprocessed_documents = {}
     for file in files:
         if file.endswith('.txt'):
             file_d = open("{}/{}".format(direct, file), 'r', encoding='utf8')
             document = file_d.read()
-            words = preprocess_document(document)
+            preprocessed_documents[file] = preprocess(document)
+
+    return preprocessed_documents
 
 def main():
-    my_str = 'Tnvnswp {:5 Q., Qmggv I5 VI/ gQ Wlllmvtgllgg Merlmégyi 2 6002; Co1Am0j,0l\"¤€q0rx/ .    Assessor’s Tax Parcel ID Number:   · O 0 ‘   The County Auditor vwll rely on the information provided on    this form. The Staff will not-read the document to verily the `     Accuracy or completeness of the indexing'
+    # my_str = 'Tnvnswp {:5 Q., Qmggv I5 VI/ gQ Wlllmvtgllgg Merlmégyi 2 6002; Co1Am0j,0l\"¤€q0rx/ .    Assessor’s Tax Parcel ID Number:   · O 0 ‘   The County Auditor vwll rely on the information provided on    this form. The Staff will not-read the document to verily the `     Accuracy or completeness of the indexing'
 
-    words = preprocess_document(my_str)
-    print(words)
-    tokens = create_bag_of_words(words)
-    print(tokens)
+    # words = preprocess(my_str)
+    # print(words)
+    # tokens = create_bag_of_words(words)
+    # print(tokens)
 
-    # parse_documents('data/DR')
-    # parse_documents('data/DT')
-    # parse_documents('data/L')
+    documents = {}
+
+    documents.update(parse_documents_from_directory('data/DR'))
+    documents.update(parse_documents_from_directory('data/DT'))
+    documents.update(parse_documents_from_directory('data/L'))
+
+    for document in documents:
+        print(document, documents[document])
 
     # print(words)
     print("Success!")
