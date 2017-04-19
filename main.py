daf9c9a5cfcbb472efaaf45bbd019e40e4279c4c
Grant Lindberg <grant.lindberg@yahoo.com>
2017-04-19 12:38:30 -0700

    Amend parser to make bag of words after parsing

--- main.py
+++ main.py
@@ -3,12 +3,7 @@
 import os
 import re
 
-def parse_document(document):
-    document = re.sub(r'\W+', ' ', document)
-    document = re.sub(r'\d+', ' ', document)
-    document = re.sub(r'\s+', ' ', document)
-
-    document = document.lower()
+def create_bag_of_words(document):
     document = document.split()
 
     tokens = {}
@@ -21,6 +16,15 @@ def parse_document(document):
 
     return tokens
 
+def parse_document(document):
+    document = re.sub(r'\W+', ' ', document)
+    document = re.sub(r'\d+', ' ', document)
+    document = re.sub(r'\s+', ' ', document)
+
+    document = document.lower()
+
+    return document
+
 def parse_documents(direct):
     files = os.listdir(direct)
     for file in files:
@@ -30,14 +34,16 @@ def parse_documents(direct):
             words = parse_document(document)
 
 def main():
-    # parse_document('data/DR/OR_Coos_2008-04-03__08003320.txt')
-    # my_str = 'Tnvnswp {:5 Q., Qmggv I5 VI/ gQ Wlllmvtgllgg Merlmégyi 2 6002; Co1Am0j,0l\"¤€q0rx/ .    Assessor’s Tax Parcel ID Number:   · O 0 ‘   The County Auditor vwll rely on the information provided on    this form. The Staff will not-read the document to verily the `     Accuracy or completeness of the indexing'
+    my_str = 'Tnvnswp {:5 Q., Qmggv I5 VI/ gQ Wlllmvtgllgg Merlmégyi 2 6002; Co1Am0j,0l\"¤€q0rx/ .    Assessor’s Tax Parcel ID Number:   · O 0 ‘   The County Auditor vwll rely on the information provided on    this form. The Staff will not-read the document to verily the `     Accuracy or completeness of the indexing'
 
-    # words = parse_document(my_str)
+    words = parse_document(my_str)
+    print(words)
+    tokens = create_bag_of_words(words)
+    print(tokens)
 
-    parse_documents('data/DR')
-    parse_documents('data/DT')
-    parse_documents('data/L')
+    # parse_documents('data/DR')
+    # parse_documents('data/DT')
+    # parse_documents('data/L')
 
     # print(words)
     print("Success!")
