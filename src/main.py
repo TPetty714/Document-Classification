2f95a009d72d82cd11fc80f4f8b597272b760f88
Tyler Dean Petty <tyler.petty@wsu.edu>
2017-04-28 06:51:13 -0700

    Finished Perceptrons

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import datetime
 from strategies import *
 from strategies import *
 from parser import *
@@ -19,7 +18,7 @@ def write_final_results_to_file(strategy, test_results, mode):
 
 def display_help():
     print('\nIncorrect usage!\n')
-    print('Type \'python3 main.py \'training_dir_1\' \'training_dir_2\' \'training_dir_3\' \'testing_file\'\n')
+    print('Type \'python3 main.py \'training_dir_1\' \'training_dir_2\' \'training_dir_3\' \'testing_file\' \'keepPreceptronWeights(yes/no)\n')
     print('Where training_dir represents a directory to train the algorithms')
     print('and testing_dir represents a directory with data to test the algorithms\n')
 
@@ -31,7 +30,7 @@ def main():
         sys.exit()
 
     actual = parse_actual_results('../data/test-results.txt')
-
+    keep = 'no'
 
     argv_iter = iter(sys.argv)
     next(argv_iter)
@@ -45,72 +44,90 @@ def main():
             normalized_L = parse_documents_from_directory(arg)
         elif arg.endswith('TEST'):
             test_results = parse_documents_from_directory(arg)
+        elif arg.endswith('yes'):
+            keep = 'yes'
         else:
             print('One or more of the directories you provided is not useful for training or testing\n')
             display_help
             sys.exit()
 
 
-    expected = classify_with_intelligrep(test_results)
-    test_strategy('intelli-grep', actual, expected)
-    write_final_results_to_file('I', expected, 'w')
-
-    expected = train_tf_idf(normalized_DR, normalized_DT, normalized_L, test_results)
-    test_strategy('if-idf', expected, actual)
-    write_final_results_to_file('T', expected, 'a')
+    # expected = classify_with_intelligrep(test_results)
+    # test_strategy('intelli-grep', actual, expected)
+    # write_final_results_to_file('I', expected, 'w')
+    #
+    # expected = train_tf_idf(normalized_DR, normalized_DT, normalized_L, test_results)
+    # test_strategy('if-idf', expected, actual)
+    # write_final_results_to_file('T', expected, 'a')
 
 #     Perceptron
-    print('Parsing for perceptrons')
-    DR = parse_documents_from_directory(sys.argv[1])
-    DRW = perceptron.wordBag(DR)
-    DRF = perceptron.featureSet(DR)
+#     print('Parsing for perceptrons')
+    #Attempt to load weights and bias if files are available
+    try:
+        resultsDR = (pickle.load(open('../data/weightsDR.p', 'rb')), pickle.load(open('../data/biasDR.p', 'rb')))
+    except FileNotFoundError or EOFError:
+        resultsDR = ({}, -0.5)
+    try:
+        resultsDT = (pickle.load(open('../data/weightsDT.p', 'rb')), pickle.load(open('../data/biasDT.p', 'rb')))
+    except FileNotFoundError or EOFError:
+        resultsDT = ({}, -0.5)
+    try:
+        resultsL = (pickle.load(open('../data/weightsL.p', 'rb')), pickle.load(open('../data/biasL.p', 'rb')))
+    except FileNotFoundError or EOFError:
+        resultsL = ({}, -0.5)
 
-    DT = parse_documents_from_directory(sys.argv[2])
-    DTW = perceptron.wordBag(DT)
-    DTF = perceptron.featureSet(DT)
+    for i in range(10):
+        Training = perceptron.setCombiner(normalized_DR, normalized_DT, normalized_L)
+        DRF = perceptron.featureSet(normalized_DR)
+        DTF = perceptron.featureSet(normalized_DT)
+        LF = perceptron.featureSet(normalized_L)
+        TrainingF = perceptron.setCombiner(DRF, DTF, LF)
+        TrainingW = perceptron.createWordBag(Training, TrainingF)
+        TestW = perceptron.createWordBag(test_results, TrainingF)
 
-    L = parse_documents_from_directory(sys.argv[3])
-    LW = perceptron.wordBag(L)
-    LF = perceptron.featureSet(L)
+        # print(str(i + 1) + ' Training perceptrons')
 
-    test = parse_documents_from_directory(sys.argv[4])
-    testW = perceptron.wordBag(test)
+        # print('Training for DR')
+        if keep != 'yes':
+            resultsDR = ({},-0.5)
+            resultsDT = ({},-0.5)
+            resultsL = ({},-0.5)
 
-    Training = perceptron.setCombiner(DR, DT, L)
-    TrainingF = perceptron.Combiner(DRF, DTF, LF)
-    TrainingW = perceptron.Combiner(DRW, DTW, LW)
-    # print('Training')
-    print('0 Training perceptrons')
-    # resultsDR = perceptron.learning(Training, TrainingW, TrainingF, DR, 'DR')
-    # resultsDT = perceptron.learning(Training, TrainingW, TrainingF, DT, 'DT')
-    # resultsL = perceptron.learning(Training, TrainingW, TrainingF, L, 'L')
-    for i in range(100):
-        Training = perceptron.setCombiner(DR, DT, L)
-        TrainingF = perceptron.Combiner(DRF, DTF, LF)
-        TrainingW = perceptron.Combiner(DRW, DTW, LW)
-        print(str(i + 1) + ' Training perceptrons')
-        resultsDR = perceptron.learning(Training, TrainingW, TrainingF, DR, 'DR',
-                                        pickle.load(open('../data/weightsDR.p', 'rb')),
-                                        pickle.load(open('../data/biasDR.p', 'rb')))
-        resultsDT = perceptron.learning(Training, TrainingW, TrainingF, DT, 'DT',
-                                        pickle.load(open('../data/weightsDT.p', 'rb')),
-                                        pickle.load(open('../data/biasDT.p', 'rb')))
-        resultsL = perceptron.learning(Training, TrainingW, TrainingF, L, 'L',
-                                       pickle.load(open('../data/weightsL.p', 'rb')),
-                                       pickle.load(open('../data/biasL.p', 'rb')))
-        # perceptron.printWeights(pickle.load(open('../data/weightsDR.p', 'rb')))
-        # perceptron.learning(Training, TrainingW, DRF, DR, 'DR',pickle.load(open('../data/weights.p', 'rb')),pickle.load(open('../data/bias.p', 'rb')),pickle.load(open('../data/alpha.p', 'rb')))
-    print('Testing perceptrons')
-    DRVotes = perceptron.testing(test, testW, TrainingF, actual, 'DR', pickle.load(open('../data/weightsDR.p', 'rb')),
-                                 pickle.load(open('../data/biasDR.p', 'rb')))
-    DTVotes = perceptron.testing(test, testW, TrainingF, actual, 'DT', pickle.load(open('../data/weightsDT.p', 'rb')),
-                                 pickle.load(open('../data/biasDT.p', 'rb')))
-    LVotes = perceptron.testing(test, testW, TrainingF, actual, 'L', pickle.load(open('../data/weightsL.p', 'rb')),
-                                pickle.load(open('../data/biasL.p', 'rb')))
-    # print('Calculating votes')
-    votes = perceptron.voteTally(DRVotes, DTVotes, LVotes, test, actual)
-    test_strategy('perceptron', votes, expected)
-    write_final_results_to_file('P', votes, 'a')
+        resultsDR = perceptron.learning(Training, TrainingW, normalized_DR,
+                                        resultsDR[0],
+                                        resultsDR[1])
+        # print('Training for DT')
+        resultsDT = perceptron.learning(Training, TrainingW, normalized_DT,
+                                        resultsDT[0],
+                                        resultsDT[1])
+        # print('Training for L')
+        resultsL = perceptron.learning(Training, TrainingW, normalized_L,
+                                       resultsL[0],
+                                       resultsL[1])
+        if keep == 'yes':
+            pickle.dump(resultsDR[0], open('../data/weightsDR.p', 'wb'))
+            pickle.dump(resultsDR[1], open('../data/biasDR.p', 'wb'))
+            pickle.dump(resultsDT[0], open('../data/weightsDT.p', 'wb'))
+            pickle.dump(resultsDT[1], open('../data/biasDT.p', 'wb'))
+            pickle.dump(resultsL[0], open('../data/weightsL.p', 'wb'))
+            pickle.dump(resultsL[1], open('../data/biasL.p', 'wb'))
+
+        DRVotes = perceptron.testing(test_results, TestW,
+                                     resultsDR[0],
+                                     resultsDR[1])
+        DTVotes = perceptron.testing(test_results, TestW,
+                                     resultsDT[0],
+                                     resultsDT[1])
+        LVotes = perceptron.testing(test_results, TestW,
+                                    resultsL[0],
+                                    resultsL[1])
+
+        # print('Calculating votes')
+        votes = perceptron.voteTally(DRVotes, DTVotes, LVotes)
+
+        perceptron.checkVotes(votes, actual)
+        # test_strategy('perceptron', votes, actual)
+        # write_final_results_to_file('P', votes, 'a')
 
 
 
